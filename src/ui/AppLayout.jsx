8bec06462407c70dfeab627ea503e7ad28b0285d
“Jahrulo” <rayjahrulo1@gmail.com>
2024-03-18 13:03:29 +0000

    changes

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+
+// THE PARENT ROUTE
+function AppLayout() {
+    return (
+        <div>
+            <Header />
+
+            <main>
+                <h1>Welcome to Resto</h1>
+                <Outlet />
+            </main>
+
+            <CartOverview />
+        </div>
+    )
+}
+
+export default AppLayout
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+          <Link to="/"> Resto App</Link>
+        </header>
+    )
+}
+
+export default Header

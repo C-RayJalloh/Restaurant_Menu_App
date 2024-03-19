93154c68ac725a01dd2b44ea72238b74f2d2c036
“Jahrulo” <rayjahrulo1@gmail.com>
2024-03-19 15:22:00 +0000

    changes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,22 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { useEffect, useState } from "react";
+
 
 
 // THE PARENT ROUTE
 function AppLayout() {
-    const [apiData, setApiData] = useState([]);
+    // const [apiData, setApiData] = useState([]);
 
-    useEffect(() => {
-      fetch("/api/data")
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data); // Optional: Log the fetched data
-          setApiData(data.menu); // Set the fetched data to the state variable
-        })
-        .catch((error) => console.error('Error fetching data:', error)); // Handle fetch errors
-    }, []);
+    // useEffect(() => {
+    //   fetch("/api/data")
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       console.log(data); // Optional: Log the fetched data
+    //       setApiData(data.menu); // Set the fetched data to the state variable
+    //     })
+    //     .catch((error) => console.error('Error fetching data:', error)); // Handle fetch errors
+    // }, []);
   
     return (
       <div>
@@ -24,16 +24,8 @@ function AppLayout() {
         <Header />
         <main>
           {/* Render the fetched data */}
-          <h1>Data from API:</h1>
-          <ul>
-          {Array.isArray(apiData) && apiData.map((item, index) => (
-  <li key={index}>
-    {/* Render the properties of each item */}
-    <p>Name: {item.menu.name}</p>
-    {/* Add more properties as needed */}
-  </li>
-))}
-          </ul>
+          <h1>Welcome</h1>
+    
           <Outlet />
         </main>
         <CartOverview />

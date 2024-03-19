85f990b2e0f0b9f87786f0316355084cae2dfa5b
“Jahrulo” <rayjahrulo1@gmail.com>
2024-03-19 11:30:34 +0000

    changes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,17 +1,25 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import { useEffect, useState } from "react";
 
 
 // THE PARENT ROUTE
 function AppLayout() {
+ const [ apiData, setapiData] = useState("testing")
+
+  useEffect( () => {
+    fetch("/api/data")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  })
 
     return (
         <div>
             <Header />
            
             <main>
-                <h1>Welcome to resto</h1>
+                <h1>{apiData}</h1>
                 <Outlet />
             </main>
 

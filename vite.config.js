dbf47d2fe050a445a4575aa1f45e0faa63a03371
“Jahrulo” <rayjahrulo1@gmail.com>
2024-03-19 11:31:35 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // AVOID CORS ISSUE
   server: {
     proxy: {
       '/api': 'https://menu-apiendpoint.onrender.com'

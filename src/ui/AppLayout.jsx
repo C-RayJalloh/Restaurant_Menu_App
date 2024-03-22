348223b8b61a49c1b27af476cc48af152cf96b1d
“Jahrulo” <rayjahrulo1@gmail.com>
2024-03-22 10:25:03 +0000

    changes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loading from './Loading';
 
 
 
 // THE PARENT ROUTE
 function AppLayout() {
-   
+   const navigation = useNavigate();
+  const isLoading = navigation.state === 'laoding';
   
     return (
-      <div>
-        {/* Your JSX */}
+      <div className="layout">
+    {isLoading && <Loading />}
+
+        {/* LAYOUT */}
         <Header />
         <main>
           {/* Render the fetched data */}
---
+++ src/ui/Loading.jsx
@@ -0,0 +1,11 @@
+// LOADING SPINNER WITH USENAVIGATION HOOK
+function Loading() {
+
+    return (
+        <div className="loader"> 
+            
+        </div>
+    )
+}
+
+export default Loading

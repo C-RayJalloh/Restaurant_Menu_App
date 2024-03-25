74a6832eaf21e0ed3d60a68b5314f625491b4b51
“Jahrulo” <mserayjay@gmail.com>
2024-03-25 21:10:48 +0000

    changes

--- src/apis/apiRestaurant.js
+++ src/apis/apiRestaurant.js
@@ -1,18 +1,6 @@
 
-// const API_URL = "/api";
 
-// export async function fetchData() {
-  //   const response = await fetch('https://menu-apiendpoint.onrender.com/api/data');
-  //   if (!response.ok) {
-    //     throw new Error('Failed to fetch data');
-    //   }
-    //   return response.json();
-    // }
-    
-    // https://react-fast-pizza-api.onrender.com/api
-
-
-    const API_URL = '/api';
+const API_URL = '/api';
 
 export async function getMenu() {
 
